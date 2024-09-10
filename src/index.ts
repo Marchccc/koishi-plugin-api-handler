a85c9606a74e49830fbabf906c2ee15ff596a18d
Marchccc <laosql@163.com>
2024-09-10 16:08:31 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Context, Schema, Logger } from 'koishi'
 
 export const usage = `
-## koishi-plugin-api-handler v1.1.0
+## koishi-plugin-api-handler v1.1.2
 
 1. 配置API地址。
 
@@ -12,6 +12,7 @@ export const usage = `
 4. 服务器需返回一个字符串，该字符串将作为机器人的回复消息。
 
 注意：只有群内@机器人的消息才会处理。
+注意：正则配置适用于群内和私聊。
 
 
 微信配合adapter-wechat4u使用。
@@ -29,6 +30,10 @@ export interface Config {
   prefix_2: string,
   prefix_3?: string,
   prefix_4?: string,
+  regex_1: string,
+  regex_2: string,
+  regex_3?: string,
+  regex_4?: string,
   wx_api?: string,
   wx_token?: string,
   bot_name?: string,
@@ -42,6 +47,10 @@ export const Config: Schema<Config> = Schema.intersect([
     prefix_2: Schema.string().description('匹配消息前缀2'),
     prefix_3: Schema.string().description('匹配消息前缀3'),
     prefix_4: Schema.string().description('匹配消息前缀4'),
+    regex_1: Schema.string().description('正则匹配消息1').description('正则匹配消息，符合则将用户消息发送到API，由API返回回复内容字符串'),
+    regex_2: Schema.string().description('正则匹配消息2'),
+    regex_3: Schema.string().description('正则匹配消息3'),
+    regex_4: Schema.string().description('正则匹配消息4'),
   }).description('基础设置'),
   Schema.object({
     wx_api: Schema.string().description('API地址'),
@@ -73,7 +82,7 @@ export function startsWithPrefix(str: string, prefix: string) {
 }
 
 export function handlePrefixes(sessionContent: string, config: Config) {
-  for (let i = 1; i <= 4; i++) {  // 假设有四个前缀
+  for (let i = 1; i <= 4; i++) {
     let prefixKey = `prefix_${i}`;
     if (config[prefixKey]) {
       if (startsWithPrefix(sessionContent, config[prefixKey])) {
@@ -82,6 +91,18 @@ export function handlePrefixes(sessionContent: string, config: Config) {
       }
     }
   }
+}
+
+export function handleRegex(sessionContent: string, config: Config) {
+  for (let i = 1; i <= 4; i++) {
+    let prefixKey = `regex_${i}`;
+    if (config[prefixKey]) {
+      if ((new RegExp(config[prefixKey])).test(sessionContent)) {
+        return `匹配正则${i}`;
+      }
+    }
+  }
+
 }
 
 export function apply(ctx: Context, config: Config) {
@@ -155,14 +176,34 @@ export function apply(ctx: Context, config: Config) {
 
       // 私聊直接返回AI内容
       if (isPrivateChat) {
+
+        // 私聊也处理正则
+        let match_regex = handleRegex(cleanContent, config)
+        if (match_regex) {
+          logger.info(match_regex)
+          const res = await ctx.http.post(config.api, {
+            token: config.token,
+            message: cleanContent,
+            channelId: session.channelId,
+          })
+            .catch((err) => {
+              return { error: err.message }
+            })
+          if (res !== undefined) {
+            return res;
+          }
+        }
+
         return next()
       }
 
       // 群内@，如果匹配到前缀，请求API结果
       // 如未匹配到，返回AI内容
       let match_prefix = handlePrefixes(cleanContent, config)
-      if (match_prefix) {
+      let match_regex = handleRegex(cleanContent, config)
+      if (match_prefix || match_regex) {
         logger.info(match_prefix)
+        logger.info(match_regex)
         const res = await ctx.http.post(config.api, {
           token: config.token,
           message: cleanContent,
